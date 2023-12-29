91617c720ebe8f8191ce07c6e555f8b7db5c0bc7
asgar007 <90379845+asgar007@users.noreply.github.com>
2023-12-29 11:42:58 +0530

    Delete Task Management/src directory

--- Task Management/src/main/java/com/assignment/task/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.task.repository;
-
-import com.assignment.task.model.Card;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CardRepository extends MongoRepository<Card, String> {
-}
